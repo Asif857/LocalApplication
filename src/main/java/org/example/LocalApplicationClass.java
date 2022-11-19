88f9f700d9579a9402f5c5a486f167e2775c2e8e
assiph <asif857@gmail.com>
2022-11-19 19:44:26 +0200

    Finished Local application?

--- src/main/java/org/example/LocalApplicationClass.java
+++ src/main/java/org/example/LocalApplicationClass.java
@@ -1,37 +1,50 @@
 package org.example;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
 import net.lingala.zip4j.ZipFile;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.apache.commons.io.FileUtils;
-import java.io.File;
-import java.io.IOException;
+
+import javax.swing.text.html.HTML;
+import java.io.*;
 import java.nio.file.Paths;
-import java.util.Base64;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class LocalApplicationClass {
-    String html; // NEED TO CHANGE TYPE!
-    final String inputFilePath;
-    final String htmlOutputPath;
-    final boolean terminate;
-    String id = UUID.randomUUID().toString();
-    final AmazonEC2 ec2;
-    final int workerRatio;
-    final BasicAWSCredentials awsCredentials;
-    public LocalApplicationClass(String inputFilePath,String htmlOutputPath,int workerRatio, boolean terminate){
+    private String html; // NEED TO CHANGE TYPE!
+    final private File inputFile;
+    final private String htmlOutputPath;
+    final private String s3Path;
+    final private boolean terminate;
+    final private String projectBucketToString;
+    private String  id;
+    final private AmazonEC2 ec2Client;
+    final private String sqsToManagerURL = "https://sqs.us-east-1.amazonaws.com/712064767285/LocalApplicationToManagerS3URLToDataSQS.fifo";
+    final private String sqsToLocalApplicationURL = "https://sqs.us-east-1.amazonaws.com/712064767285/ManagerToLocalApplicationSQS.fifo";
+
+    final private AmazonSQS sqsClient;
+    final private int workerRatio;
+    final private AmazonS3 s3Client;
+    public LocalApplicationClass(String inputFilePath,String htmlOutputPath,int workerRatio, boolean terminate) throws GitAPIException, IOException {
         this.terminate = terminate;
-        this.inputFilePath = inputFilePath;
+        this.inputFile = new File(inputFilePath);
         this.workerRatio = workerRatio;
         this.htmlOutputPath = htmlOutputPath;
-        this.awsCredentials = new BasicAWSCredentials("ASIA2LSS6CU2VC3Z464H","AysZRyin+sLpFa9N3rsXLDm8CeoEtqr75SgxR+UQ");
-        ec2 = AmazonEC2ClientBuilder.standard().withCredentials((new AWSStaticCredentialsProvider(awsCredentials))).build();
+        this.id = "5"; //UUID.randomUUID().toString();
+        this.s3Path = "Input/" + id + "/" + "LinksFile";
+        this.projectBucketToString = "amazon-first-project";
+        setCredentials();
+        ec2Client = AmazonEC2ClientBuilder.defaultClient();
+        s3Client = AmazonS3ClientBuilder.defaultClient();
+        sqsClient = AmazonSQSClientBuilder.defaultClient();
     }
     public void startManager() {
         if (!checkIfManagerIsUp()) {
@@ -43,12 +56,12 @@ public class LocalApplicationClass {
                     .withUserData((Base64.getEncoder().encodeToString("/*your USER DATA script string*/".getBytes())));
             Reservation managerReservation = new Reservation();
             managerReservation.setRequesterId("manager");
-            ec2.runInstances(runRequest).withReservation(managerReservation);
+            ec2Client.runInstances(runRequest).withReservation(managerReservation);
         }
     }
     public boolean checkIfManagerIsUp(){
         DescribeInstancesRequest request = new DescribeInstancesRequest();
-        DescribeInstancesResult response = ec2.describeInstances(request);
+        DescribeInstancesResult response = ec2Client.describeInstances(request);
         boolean done = false;
         while (!done) {
             List<Reservation> reserveList = response.getReservations();
@@ -64,29 +77,72 @@ public class LocalApplicationClass {
         }
         return false;
     }
-    public void uploadFilesToS3(){
-        createOutputInputFolderInS3();
+    public void uploadFileToS3(){
+        s3Client.putObject(projectBucketToString,s3Path,inputFile);
     }
-    public void createOutputInputFolderInS3(){
-
-
+    public void putInLocalToManagerSQS(){
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+        messageAttributes.put(id, new MessageAttributeValue()
+                .withStringValue(projectBucketToString + "/" + s3Path)
+                .withDataType("String"));
+        SendMessageRequest requestMessageSend = new SendMessageRequest()
+                .withQueueUrl(sqsToManagerURL)
+                .withMessageAttributes(messageAttributes)
+                .withMessageDeduplicationId(s3Path)
+                .withMessageGroupId(id);
+        SendMessageResult result = sqsClient.sendMessage(requestMessageSend);
+        System.out.println(result.getMessageId());
     }
-    public void putInSQS(String sqsURL){
 
+    public Message awaitMessageFromManagerToLocalApplicationSQS() {
+        while (true) {
+            ReceiveMessageRequest request = new ReceiveMessageRequest()
+                    .withQueueUrl(sqsToLocalApplicationURL)
+                    .withMaxNumberOfMessages(1)
+                    .withMessageAttributeNames("All");
+            List<Message> messages = sqsClient.receiveMessage(request).getMessages();
+           MessageAttributeValue messageURL = messages.get(0).getMessageAttributes().get(id);
+            if (messageURL != null){
+                System.out.println(messages.get(0));
+                return messages.get(0);
+            }
+        }
     }
-    public void awaitMessageFromSQS(String sqsURL){
 
+    public void deleteMessage(Message message){
+        sqsClient.deleteMessage(sqsToLocalApplicationURL,message.getReceiptHandle());
     }
-    public void updateHtmlField(String message){
+    public void createHtml(File outputFile) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("/home/assiph/IdeaProjects/LocalApplication/src/main/java/Output/Output.html"));
+        String firstLine = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n";
+        String secondLine = "</head><body>\n";
+        bw.write(firstLine);
+        bw.write(secondLine);
+        try (BufferedReader br = new BufferedReader(new FileReader(outputFile))) {
+            String openingP = "<p>\n";
+            String closingP = "</p>\n";
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(openingP);
+                bw.write("    <img src=\"" + line + "\"" + "><br>\n");
+                line = br.readLine();
+                bw.write("    " + line + "\n");
+                bw.write(closingP);
+            }
+            bw.write("</body></html>\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        bw.close();
+    }
 
-    }
-    public void setCredentials() throws IOException, GitAPIException {
+    private void setCredentials() throws IOException, GitAPIException {
         String home = System.getProperty("user.home");
         Git.cloneRepository()
                 .setURI("https://github.com/Asif857/NotCreds.git")
-                .setDirectory(Paths.get(home + "/IdeaProjects/Worker/src/main/creds").toFile())
+                .setDirectory(Paths.get(home + "/IdeaProjects/LocalApplication/src/main/creds").toFile())
                 .call();
-        String zipFilePath = home + "/IdeaProjects/Worker/src/main/creds/aws_creds.zip";
+        String zipFilePath = home + "/IdeaProjects/LocalApplication/src/main/creds/aws_creds.zip";
         String destDir = home + "/.aws";
         unzip(zipFilePath, destDir);
         deleteDirectory();
@@ -97,8 +153,19 @@ public class LocalApplicationClass {
         zipFile.extractAll(destDir);
     }
     private void deleteDirectory() throws IOException {
-        FileUtils.deleteDirectory(new File("/home/assiph/IdeaProjects/Worker/src/main/creds"));
+        FileUtils.deleteDirectory(new File("/home/assiph/IdeaProjects/LocalApplication/src/main/creds"));
     }
 
+    public String getId(){
+        return this.id;
+    }
 
+    public File getFileFromS3(String messageS3Path) {
+        String outputPath = "Output/" + messageS3Path;
+        System.out.println(outputPath);
+        String home = System.getProperty("user.home");
+        File outputFile = new File (home + "/IdeaProjects/LocalApplication/src/main/java/Output/outputFile.txt");
+        s3Client.getObject(new GetObjectRequest(projectBucketToString,outputPath),outputFile);
+        return outputFile;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,21 @@
 package org.example;
 
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws GitAPIException, IOException {
-        LocalApplicationClass localApplication = new LocalApplicationClass(args[1],args[2],Integer.parseInt(args[3]),Boolean.parseBoolean(args[4]));
-        localApplication.setCredentials();
-
-
+        //LocalApplicationClass localApplication = new LocalApplicationClass(args[1],args[2],Integer.parseInt(args[3]),Boolean.parseBoolean(args[4]));
+        LocalApplicationClass localApplication = new LocalApplicationClass("/home/assiph/Desktop/key/input-example.txt","args[2]",4,false);
+        localApplication.uploadFileToS3();
+        localApplication.putInLocalToManagerSQS();
+        Message message = localApplication.awaitMessageFromManagerToLocalApplicationSQS();
+        String messageS3Path = message.getMessageAttributes().get(localApplication.getId()).getStringValue();
+        File outputFile = localApplication.getFileFromS3(messageS3Path);
+        localApplication.createHtml(outputFile);
     }
 }
